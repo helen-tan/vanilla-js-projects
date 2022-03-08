56fbd54045e23c489c87599f189b1d02c83016da
Helen Tan <helentanmz0909@gmail.com>
2022-03-08 16:33:05 +0800

    validate blank fields

---
+++ form-validator/script.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+// Show input error message
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+// Show success outline
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+
+    return re.test(String(email).toLowerCase());
+}
+
+// check required fields
+function checkRequired(inputArr){
+    inputArr.forEach(function(input) {
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+            
+    });
+}
+
+// Get field name
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// Event Listeners
+form.addEventListener('submit', function(e) {
+    checkRequired([username, email, password, password2]);
+
+    e.preventDefault();
+});
