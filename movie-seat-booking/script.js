4a475f14239f9b30b0a21690da861193451723de
Helen Tan <helentanmz0909@gmail.com>
2022-03-10 18:19:58 +0800

    Enable seat count and price update in UI

--- movie-seat-booking/script.js
+++ movie-seat-booking/script.js
@@ -5,13 +5,33 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
-const ticketPrice = +movieSelect.value; // + changes the string to a number (like parseInt)
+let ticketPrice = +movieSelect.value; // + changes the string to a number (like parseInt)
 
+// update total and count
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected'); // will store a NodeList of the selected seats
+
+    const selectedSeatsCount = selectedSeats.length;
+    
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Event Listener
+// Seat click event
 container.addEventListener('click', (e) => {
-    // select an unoccupied seat
+    // select an unoccupied seat (event delegation)
     if(e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
         e.target.classList.toggle('selected');
+
+        updateSelectedCount();
     }
+});
+
+// Movie select event
+movieSelect.addEventListener('change', (e) => {
+    ticketPrice = +e.target.value;
+    updateSelectedCount();
 });
 
 
