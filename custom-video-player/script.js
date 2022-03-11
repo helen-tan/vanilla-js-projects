ff28dc5c334a79be1396ebd71db6949dd0b49a9a
Helen Tan <helentanmz0909@gmail.com>
2022-03-11 17:30:40 +0800

    Enable timestamp update

--- custom-video-player/script.js
+++ custom-video-player/script.js
@@ -25,6 +25,20 @@ function updatePlayIcon() {
 // Update progress & timestamp
 function updateProgress() {
     progress.value = (video.currentTime / video.duration) * 100;
+
+    // Get minutes
+    let mins = Math.floor(video.currentTime / 60);
+    if(mins < 10){
+        mins = '0' + String(mins);
+    }
+    // Get seconds
+    let secs = Math.floor(video.currentTime % 60);
+    if(secs < 10){
+        secs = '0' + String(secs);
+    }
+
+    timestamp.innerHTML = `${mins}:${secs}`;
+
 }
 
 // Set video time to progress
