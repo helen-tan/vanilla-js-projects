e87c5feae2b209458621e3940fdecb368ac11415
Helen Tan <helentanmz0909@gmail.com>
2022-03-15 16:09:19 +0800

    Fetch user data from Random User Ganerator API

---
+++ dom-array-methods/script.js
@@ -0,0 +1,34 @@
+const main = document.getElementById('main');
+const addUserBtn = document.getElementById('add-user');
+const doubleBtn = document.getElementById('double');
+const showMillionairesBtn = document.getElementById('show-millionaires');
+const sortBtn = document.getElementById('sort');
+const calculateWeathBtn = document.getElementById('calculate-wealth');
+
+// People data
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// Fetch random user and add money
+async function getRandomUser() {
+    const res = await fetch('https://randomuser.me/api');
+    const data = await res.json();
+
+    const user = data.results[0];
+
+    const newUser = {
+        name: `${user.name.first} ${user.name.last}`,
+        money: Math.floor(Math.random() * 1000000)
+    }
+
+    addData(newUser);
+}
+
+// Add new user object to data array
+function addData(obj) {
+    data.push(obj);
+}
+
