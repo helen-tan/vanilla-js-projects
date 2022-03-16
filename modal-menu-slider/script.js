4919edf48fb18979aa76af68e63bdb9c47d3441b
Helen Tan <helentanmz0909@gmail.com>
2022-03-16 16:12:28 +0800

    Enable modal toggle

--- modal-menu-slider/script.js
+++ modal-menu-slider/script.js
@@ -7,3 +7,18 @@ const modal = document.getElementById('modal');
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav');
 });
+
+// Show Modal
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+});
+
+// Hide Modal
+close.addEventListener('click', () => {
+    modal.classList.remove('show-modal');
+});
+
+// Hide Modal on outside click
+window.addEventListener('click', e => {
+    e.target == modal ? modal.classList.remove('show-modal') : false;
+});
