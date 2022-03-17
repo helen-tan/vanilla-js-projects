99aec899d830465f7a544a3aee81def658b00a2b
Helen Tan <helentanmz0909@gmail.com>
2022-03-17 16:33:00 +0800

    Letter press event handler

--- hangman-game/script.js
+++ hangman-game/script.js
@@ -17,7 +17,7 @@ const correctLetter = ['r'];
 const wrongLetters = [];
 
 // Show a random hidden word
-function displayWord() {
+function displayWord(){
     wordEl.innerHTML = `
         ${selectedWord
             .split('')
@@ -31,10 +31,42 @@ function displayWord() {
 
     const innerWord = wordEl.innerText.replace(/\n/g, '');
 
-    if(innerWord === selectedWord) {
+    if (innerWord === selectedWord) {
         finalMessage.innerText = `Congratulations! You won!`;
         popup.style.display = 'flex';
     }
 }
+
+// Update the wrong letters
+function updateWrongLettersEl(){
+    console.log('Update wrong');
+}
+
+// Keydown letter press
+window.addEventListener('keydown', e => {
+    // Each key has a keycode. Letters a-z is 65 - 90.
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key;
+
+        // Check if letter is in the selected word
+        if (selectedWord.includes(letter)) {
+            // Check if letter already exist in the correctLetters array
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter);
+                displayWord();
+            } else {
+                showNotification();
+            }
+        } else { // if letter is not in selected word, if not in wrongletters arr
+            if (!wrongLetters.includes(letter)) {
+                wrongLetters.push(letter);
+
+                updateWrongLettersEl();
+            } else { // if wrong letter has already been pressed (exist in wrongLetters arr)
+                showNotification();
+            }
+        }
+    }
+});
 
 displayWord();
