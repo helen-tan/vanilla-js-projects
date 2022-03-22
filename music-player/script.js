ade340d85554cea2eebc62edcf4efaa863865b28
Helen Tan <helentanmz0909@gmail.com>
2022-03-22 17:48:19 +0800

    Next song to play when current song ends

--- music-player/script.js
+++ music-player/script.js
@@ -112,3 +112,6 @@ audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
