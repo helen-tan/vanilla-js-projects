3f07df63aad2fa6955b5aab1ab51f49d6b7b2df7
Helen Tan <helentanmz0909@gmail.com>
2022-03-24 15:43:28 +0800

    Change random word when it is typed

---
+++ typing-game/script.js
@@ -0,0 +1,69 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+// List of words for game
+const words = [
+    'sigh',
+    'tense',
+    'airplane',
+    'ball',
+    'pies',
+    'juice',
+    'warlike',
+    'bad',
+    'north',
+    'dependant',
+    'steer',
+    'silver',
+    'highfalutin',
+    'superficial',
+    'quince',
+    'eight',
+    'feeble',
+    'admit',
+    'drag',
+    'loving'
+];
+
+// Init word
+let randomWord;
+
+// Init score
+let score = 0;
+
+// Init time
+let time = 10;
+
+// Generate random word from words array
+function getRandomWord() {
+    return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+    randomWord = getRandomWord();
+    word.innerHTML = randomWord;
+}
+
+addWordToDOM();
+
+// Event Listeners
+text.addEventListener('input', e => {
+    const insertedText = e.target.value;
+
+    if(insertedText === randomWord){
+        // Change the random word
+        addWordToDOM();
+
+        // Clear input
+        e.target.value = "";
+    }
+});
+
