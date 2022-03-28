c690f270db50d75af954a702a8398488bd20002a
Helen Tan <helentanmz0909@gmail.com>
2022-03-28 15:07:06 +0800

    Toggle text box

--- text-to-speech-reader/script.js
+++ text-to-speech-reader/script.js
@@ -74,3 +74,8 @@ function createBox(item) {
 
     main.appendChild(box);
 }
+
+// Toggle text box
+toggleBtn.addEventListener('click', () => 
+document.getElementById('text-box').classList.toggle('show')
+);
