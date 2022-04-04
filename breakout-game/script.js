3d0ad9637f102f2f6b190fac91fd91d57a76859c
Helen Tan <helentanmz0909@gmail.com>
2022-04-04 12:05:58 +0800

    Draw ball, paddle & score with Canvas API

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -1,6 +1,64 @@
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d'); // Create context - convention is to use 'ctx'
+
+let score = 0;
+
+// Create ball props
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+}
+
+// Create paddle props
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0
+}
+
+// Draw ball onto canvas
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Draw paddle onto canvas
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Draw score onto canvas
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+
+// Draw everything into DOM
+function draw() {
+    drawBall();
+    drawPaddle();
+    drawScore();
+}
+
+draw();
 
 // rulesBtn & closeBtn Event Handlers
 rulesBtn.addEventListener('click', () => {
