dcdbee2537e4fb522751542bc67d0b59fb434208
Helen Tan <helentanmz0909@gmail.com>
2022-04-04 11:40:48 +0800

    Added styles

---
+++ breakout-game/script.js
@@ -0,0 +1,12 @@
+const rulesBtn = document.getElementById('rules-btn');
+const closeBtn = document.getElementById('close-btn');
+const rules = document.getElementById('rules');
+
+// rulesBtn & closeBtn Event Handlers
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show');
+});
