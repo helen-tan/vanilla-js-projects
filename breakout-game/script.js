192a7bc3107d21880905e127d8ec0dd3a9d19d46
Helen Tan <helentanmz0909@gmail.com>
2022-04-04 14:39:34 +0800

    Move paddle

--- breakout-game/script.js
+++ breakout-game/script.js
@@ -89,15 +89,62 @@ function drawBricks() {
     });
 }
 
+// Move paddle on canvas
+function movePaddle() {
+    paddle.x += paddle.dx;
+
+    // Wall detection
+    if(paddle.x + paddle.w > canvas.width) {
+        paddle.x = canvas.width - paddle.w;
+    }
+
+    if(paddle.x < 0) {
+        paddle.x = 0;
+    }
+}
+
 // Draw everything into DOM
 function draw() {
+    // clear canvas first
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
     drawBall();
     drawPaddle();
     drawScore();
     drawBricks();
 }
 
-draw();
+// Update canvas drawing and animation
+function update(){
+    movePaddle();
+
+    // Draw everything 
+    draw();
+
+    requestAnimationFrame(update);
+}
+
+update();
+
+// Keydown event
+function keyDown(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight'){
+        paddle.dx = paddle.speed;
+    } else if (e.key === 'Left' || e.key === 'ArrowLeft') {
+        paddle.dx = -paddle.speed;
+    }
+}
+
+// Keyup event
+function keyUp(e) {
+    if(e.key === 'Right' || e.key === 'ArrowRight' || e.key === 'Left' || e.key === 'ArrowLeft'){
+        paddle.dx = 0;
+    }
+}
+
+// Keyboard event handlers
+document.addEventListener('keydown', keyDown);
+document.addEventListener('keyup', keyUp);
 
 // rulesBtn & closeBtn Event Handlers
 rulesBtn.addEventListener('click', () => {
