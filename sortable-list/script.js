0807978de2c20680bb66ead73bc28c43541319ef
Helen Tan <helentanmz0909@gmail.com>
2022-04-05 11:55:43 +0800

    Styled list items

--- sortable-list/script.js
+++ sortable-list/script.js
@@ -34,7 +34,7 @@ function createList() {
             listItem.setAttribute('data-index', index);
             listItem.innerHTML = `
                 <span class="number">${index + 1}</span>
-                <div class=""draggable" draggable="true">
+                <div class="draggable" draggable="true">
                     <p class="person-name">${person}</p>
                     <i class="fas fa-grip-lines"></i>
                 </div>
