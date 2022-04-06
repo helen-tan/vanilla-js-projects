3925abe3ddc0ce3cab570cecca0611428aaf9f41
Helen Tan <helentanmz0909@gmail.com>
2022-04-06 15:14:54 +0800

    Check if user mic input is a valid num, if in range, if > or < or = to random num

--- speak-number-guess/script.js
+++ speak-number-guess/script.js
@@ -16,8 +16,46 @@ recognition.start();
 function onSpeak(e) {
     const msg = e.results[0][0].transcript;
 
-    //writeMessage(msg);
-    //checkNumber(msg);
+    writeMessage(msg);
+    checkNumber(msg);
+}
+
+// TWrite what user speaks to the DOM
+function writeMessage(msg) {
+    msgEl.innerHTML = `
+        <div>You said:</div>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check message against number
+function checkNumber(msg) {
+    const num = +msg;  // + converts it from a string to a number
+
+    // Check if valid number
+    if(Number.isNaN(num)){
+        msgEl.innerHTML = '<div>That is not a valid number</div>';
+        return;
+    }
+
+    // Check if in range
+    if(num > 100 || num < 1){
+        msgEl.innerHTML += `<div>Number must be between 1 and 100</div>`;
+        return
+    }
+
+    // Check number
+    if(num === randomNum) {
+        document.body.innerHTML = `
+            <h2>Congrats! You have guessed the number! <br><br>
+            It was ${num}</h2>
+            <button class="play-again" id="play-again">Play Again</button>
+        `;
+    } else if(num > randomNum) {
+        msgEl.innerHTML += `<div>GO LOWER</div>`;
+    } else {
+        msgEl.innerHTML += `<div>GO HIGHER</div>`;
+    }
 }
 
 // Generate random number
